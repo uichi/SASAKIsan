321a085fa1963c14a0387077dd0cdff93302021f
uichi <37263474+uichi@users.noreply.github.com>
2022-01-10 22:46:36 +0900

    Add user api.

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,9 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :attendances,         only: [:create, :edit, :update, :destroy]
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: %i[index create]
+    end
+  end
 end
